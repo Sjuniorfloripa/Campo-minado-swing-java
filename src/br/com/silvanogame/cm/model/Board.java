d6e63861fb17d1c45723b378bb3b99b6782767b0
Silvano Charão Dias Junior <sjuniorfloripa666@gmail.com>
2021-09-06 08:26:41 -0300

    projeto criado

---
+++ src/br/com/silvanogame/cm/model/Board.java
@@ -0,0 +1,83 @@
+package br.com.silvanogame.cm.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Board {
+
+    private int lines;
+    private int columns;
+    private int mines;
+
+    private final List<Field> fields = new ArrayList<>();
+
+    public Board(int lines, int columns, int mines) {
+        this.lines = lines;
+        this.columns = columns;
+        this.mines = mines;
+
+        generateFields();
+        associateNeighbors();
+        drawMines();
+    }
+
+    public void open(int line, int column) {
+        try {
+            fields.parallelStream()
+                    .filter(c -> c.getLine() == line)
+                    .filter(c -> c.getColumn() == column)
+                    .findFirst()
+                    .ifPresent(c -> c.open());
+        } catch (Exception e) {
+            //FIXME Ajustar implementação do método abrir.
+            fields.forEach(c -> c.setOpened(true));
+            throw e;
+        }
+    }
+
+    public void mark(int line, int column) {
+        fields.parallelStream()
+                .filter(c -> c.getLine() == line)
+                .filter(c -> c.getColumn() == column)
+                .findFirst()
+                .ifPresent(c -> c.alternateMarking());
+    }
+
+    private void generateFields() {
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < columns; j++) {
+                fields.add(new Field(i, j));
+            }
+        }
+    }
+
+    private void associateNeighbors() {
+        for (Field f1 : fields) {
+            for (Field f2 : fields) {
+                f1.addNeighbor(f2);
+            }
+        }
+    }
+
+    private void drawMines() {
+        long armedMines = 0;
+        Predicate<Field> mined = c -> c.isMined();
+
+        do {
+            int random = (int) (Math.random() * fields.size());
+            fields.get(random).mine();
+            armedMines = fields.stream().filter(mined).count();
+        } while (armedMines < mines);
+    }
+
+    public boolean missionAccomplished() {
+        return fields.stream().allMatch(c -> c.missionAccomplished());
+    }
+
+    public void restart() {
+        fields.stream().forEach(c -> c.restart());
+        drawMines();
+    }
+    
+}
---
+++ src/br/com/silvanogame/cm/model/Field.java
@@ -0,0 +1,111 @@
+package br.com.silvanogame.cm.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Field {
+
+    private boolean mined = false;
+    private boolean opened = false;
+    private boolean marked = false;
+    private final int line;
+    private final int column;
+    private List<Field> neighbors = new ArrayList<>();
+
+    public Field(int line, int column) {
+        this.line = line;
+        this.column = column;
+    }
+
+    public boolean addNeighbor(Field neigbor) {
+        boolean differentLine = line != neigbor.line;
+        boolean differentColumn = column != neigbor.column;
+        boolean diagonal = differentLine && differentColumn;
+
+        int deltaLine = Math.abs(line - neigbor.line);
+        int deltaColumn = Math.abs(column - neigbor.column);
+        int detaGeneral = deltaColumn + deltaLine;
+
+        if (detaGeneral == 1 && !diagonal) {
+            neighbors.add(neigbor);
+            return true;
+        } else if (detaGeneral == 2 && diagonal) {
+            neighbors.add(neigbor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void alternateMarking() {
+        if (!opened) {
+            marked = !marked;
+        }
+    }
+
+    public boolean open() {
+        if (!opened && !marked) {
+            opened = true;
+
+            if (mined) {
+                // TODO implementar nova versão.
+            }
+            if (safeNeighborhood()) {
+                neighbors.forEach(v -> v.open());
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean safeNeighborhood() {
+        return neighbors.stream().noneMatch(v -> v.mined);
+    }
+
+    public void mine(){
+        mined = true;
+    }
+
+    public boolean isMarked() {
+        return marked;
+    }
+
+    void setOpened(boolean opened){
+        this.opened = opened;
+    }
+
+    public boolean isMined() {
+        return mined;
+    }
+
+    public boolean isOpened() {
+        return opened;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+     boolean missionAccomplished() {
+        boolean unveiling = !mined && opened;
+        boolean protectedField = mined && marked;
+
+        return unveiling || protectedField;
+    }
+
+    public long minesInTheNeighborhood() {
+        return neighbors.stream().filter(v -> v.mined).count();
+    }
+
+    public void restart() {
+        opened = false;
+        mined = false;
+        marked = false;
+    }
+
+}
